221b80d8acaa249836f0b6c42e4574f1a144bb26
VaidasKr <vaidas.krisceliunas@telesoftas.com>
2023-01-06 08:47:57 +0200

    Install splash screen and update status bar colors

--- app/src/main/java/lt/vaidas/argyledemo/ui/MainActivity.kt
+++ app/src/main/java/lt/vaidas/argyledemo/ui/MainActivity.kt
@@ -3,38 +3,24 @@ package lt.vaidas.argyledemo.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import lt.vaidas.argyledemo.ui.theme.AppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         setContent {
             AppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                rememberSystemUiController().setStatusBarColor(MaterialTheme.colors.background, !isSystemInDarkTheme())
+                Surface {
+                    // content
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    AppTheme {
-        Greeting("Android")
     }
 }
