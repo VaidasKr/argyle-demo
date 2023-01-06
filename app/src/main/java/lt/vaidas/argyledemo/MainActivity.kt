829d0104c2fdb44a55591e6316bac2d09b8fc791
VaidasKr <vaidas.krisceliunas@telesoftas.com>
2023-01-06 08:40:44 +0200

    Add internet permission and set splash theme to MainActivity

--- app/src/main/java/lt/vaidas/argyledemo/MainActivity.kt
+++ app/src/main/java/lt/vaidas/argyledemo/MainActivity.kt
@@ -10,13 +10,13 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import lt.vaidas.argyledemo.ui.theme.ArgyleDemoTheme
+import lt.vaidas.argyledemo.ui.theme.AppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ArgyleDemoTheme {
+            AppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
                     Greeting("Android")
@@ -34,7 +34,7 @@ fun Greeting(name: String) {
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    ArgyleDemoTheme {
+    AppTheme {
         Greeting("Android")
     }
 }
