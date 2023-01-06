bcf49c466c5dd919bd7166754753c086e65417e0
VaidasKr <vaidas.krisceliunas@telesoftas.com>
2023-01-06 10:26:30 +0200

    Update LinksResponse.kt result to be nullable

--- app/src/main/java/lt/vaidas/argyledemo/links/LinksResponse.kt
+++ app/src/main/java/lt/vaidas/argyledemo/links/LinksResponse.kt
@@ -4,4 +4,4 @@ import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class LinksResponse(@SerialName("results") val results: List<LinkItemResponse>)
+data class LinksResponse(@SerialName("results") val results: List<LinkItemResponse>?)
